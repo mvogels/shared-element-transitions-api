42e16a78ccfc1bb149c05bb246267e27a01ef8d9
Milan Vogels <milan.vogels@iodigital.com>
2022-06-16 16:15:58 +0200

    update to support multiple grid cards

--- main.js
+++ main.js
@@ -2,13 +2,16 @@ import './style.css'
 
 const listView = document.querySelector('.list');
 const detailView =  document.querySelector('.detail');
+
 let isDetail = false;
+let currentThumbImg = null;
+let currentDetailImg = null;
 
 document.querySelectorAll('a').forEach((link) => {
-  link.addEventListener('click', (e) => navigate(e));
+  link.addEventListener('click', (e) => navigate(e, link));
 });
 
-async function navigate(e) {
+async function navigate(e, link) {
   e.preventDefault();
 
   if (!document.createDocumentTransition) {
@@ -16,19 +19,33 @@ async function navigate(e) {
     return;
   }
 
+  if (!currentThumbImg) {
+    currentThumbImg = link.querySelector('img');
+  }
+
+  currentDetailImg = detailView.querySelector('img');
+
+  if (currentThumbImg) {
+    currentThumbImg.style.pageTransitionTag = 'article-img';
+  }
+
   const transition = document.createDocumentTransition();
   await transition.start(() => toggleView());
+
+  if (!isDetail) {
+    currentThumbImg.style.pageTransitionTag = '';
+    currentThumbImg = null;
+  }
 }
 
 function toggleView() {
-  if (isDetail) {
-    listView.style.display = 'grid';
-    detailView.style.display = 'none';
+    if (isDetail) {
+      listView.style.display = 'grid';
+      detailView.style.display = 'none';
+    } else {
+      listView.style.display = 'none';
+      detailView.style.display = 'block';
+    }
 
-  } else {
-    listView.style.display = 'none';
-    detailView.style.display = 'block';
-  }
-
-  isDetail = !isDetail;
+    isDetail = !isDetail;
 }
